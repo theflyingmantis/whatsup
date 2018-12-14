e401ba360f628bb6e90b4e4aeb3f2e2e244ad002
Abhinav Rai <abhinav.rai.1996@gmail.com>
2018-12-14 15:41:03 +0530

    added vision - chrome extension dashboard with only url

--- app.py
+++ app.py
@@ -1,17 +1,185 @@
-from flask import Flask,render_template,request, flash, redirect, url_for, jsonify
+from flask import Flask,render_template,request, flash, redirect, url_for, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 import os
 import requests
 from bs4 import BeautifulSoup
 import random
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://abhinav:abhinav@localhost/whatsup'
 db = SQLAlchemy(app)
+
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+	
+class TV(db.Model):
+    __tablename__='tv'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+class TVSlide(db.Model):
+	__tablename__="tv_slide"
+	id = db.Column(db.Integer, primary_key=True, unique=True)
+	tv_id = db.Column(db.Integer)
+	url = db.Column(db.String(1000))
+	time = db.Column(db.Integer)
+	title = db.Column(db.String(100))
+	is_active = db.Column(db.Boolean, default=True)
+	is_deleted = db.Column(db.Boolean, default=False)
+
+@app.route('/tv', methods = ['GET', 'POST'])
+def tv():
+	if request.method == "POST":
+		tvname = request.values.get("tvname")
+		password = request.values.get("password")
+		tv = TV.query.filter_by(name=tvname, password=password).first()
+		if tv:
+			session['logged_in'] = tv.id
+			return redirect(url_for('tv_dashboard'))
+		flash("TV Does not exist - Please Signup First")
+	if isLoggedIn():
+		return redirect(url_for('tv_dashboard'))
+	return render_template('tv.html')
+
+@app.route('/tv/signup', methods = ['GET', 'POST'])
+def tv_signup():
+	if request.method == "POST":
+		tvname = request.values.get("tvname")
+		password = request.values.get("password")
+		tv = TV(name=tvname, password=password)
+		try:
+			db.session.add(tv)
+			db.session.commit()
+			flash("Welcome "+tvname)
+			return redirect(url_for('tv'))
+		except Exception as e:
+			exc = str(e)
+			flash(exc)
+	return render_template('tv_signup.html')
+
+def isLoggedIn():
+	if session.get('logged_in'):
+		return True
+	return False
+
+@app.route('/tv/checkforchange', methods = ['POST'])
+def tv_check_for_change():
+	clientKey = request.values.get('key')
+	tvname = request.values.get('tvname')
+	password = request.values.get('password')
+	tv = TV.query.filter_by(name=tvname, password=password).first()
+	if tv:
+		key = getActiveTVSlidesIdConcatenated(tv.id)
+		slides = [tvSlideToJson(slide.id, slide.url, slide.time) for slide in TVSlide.query.filter_by(is_deleted=False, is_active=True, tv_id=tv.id).all()]
+		obj = {
+			"isChanged": clientKey != key,
+			"key": key,
+			"data": slides
+		}
+		return jsonify(obj)
+	else:
+		obj = {
+			"isChanged": False,
+			"key": "0",
+			"data": []
+		}
+		return jsonify(obj)
+
+ 
+def tvSlideToJson(id, url, time):
+	return {
+		"id": id,
+		"url": url,
+		"time": time
+	}
+
+
+def getActiveTVSlidesIdConcatenated(id):
+	slides = TVSlide.query.filter_by(is_deleted=False, is_active=True, tv_id=id).all()
+	key = ""
+	for slide in slides:
+		key+=str(slide.id)
+	return key
+
+@app.route('/tv/logout', methods = ['GET'])
+def logout():
+	session.pop('logged_in', None)
+	flash("You Are logged out of GO-TV")
+	return redirect(url_for('tv'))
+
+@app.route('/tv/dashboard', methods = ['GET'])
+def tv_dashboard():
+	if not isLoggedIn():
+		flash("PLease Login First")
+		return redirect(url_for('tv'))
+	return render_template('tv_dashboard.html')
+
+@app.route('/tv/manage', methods = ['GET', 'POST'])
+def tv_manage():
+	if not isLoggedIn():
+		flash("PLease Login First")
+		return redirect(url_for('tv'))
+	slides = TVSlide.query.filter_by(is_deleted=False).all()
+	return render_template('manage_slides.html', slides = slides)
+
+
+@app.route('/tv/slide/<action>/<id>')
+def slide_action(action, id):
+	if action == "delete":
+		slide = TVSlide.query.get(id)
+		slide.is_deleted = True
+		db.session.commit()
+	if action == "active":
+		slide = TVSlide.query.get(id)
+		slide.is_active = True
+		db.session.commit()
+	if action == "inactive":
+		slide = TVSlide.query.get(id)
+		slide.is_active = False
+		db.session.commit()
+	return redirect(url_for('tv_manage'))
+
+
+
+@app.route('/tv/create_slide', methods = ['GET', 'POST'])
+def tv_create_slide():
+	if not isLoggedIn():
+		flash("Please Login First")
+		return redirect(url_for('tv'))
+	if request.method == "POST":
+		time = int(request.values.get('time'))*1000
+		title = request.values.get('title')
+		url = request.values.get('url')
+		slide = TVSlide(title=title, url=url, time=time, tv_id=int(session.get('logged_in')))
+		try:
+			db.session.add(slide)
+			db.session.commit()
+			flash("Slide Created")
+		except Exception as e:
+			exc = str(e)
+			flash(exc)
+		return redirect(url_for('tv_dashboard'))
+
+	# if "type" in request.args:
+	# 	if request.args['type']=="url":
+	# 		return render_template('tv/create_slide_url.html')
+	# 	if request.args['type']=="only_text":
+	# 		return render_template('create_slide_only_text.html')
+	# 	if request.args['type']=="text_and_image":
+	# 		return render_template('create_slide_text_and_image.html')
+	# 	if request.args['type']=="only_image":
+	# 		return render_template('create_slide_only_image.html')
+	# 	if request.args['type']=="text_over_image":
+	# 		return render_template('create_slide_text_over_image.html')
+	return render_template('tv/create_slide_url.html')
+
+
+# **********************************************************************************************************************
+# Whatsup
+# **********************************************************************************************************************
 
 background_color_scheme = ["#388BF2", "#E52A34", "#FBAF18", "#FC8338"]
 
@@ -28,9 +196,6 @@ class Slide(db.Model):
 	is_deleted = db.Column(db.Boolean, default=False)
 	time = db.Column(db.Integer)
 	title = db.Column(db.String(100))
-
-	# def __repr__(self):
-	# 	return '<slide %r %r %r>' % self.id, self.text, self.is_activ
 
 def slideToJson(id, type, text, image_url, background_color, background_image_url, is_active, is_deleted, time, title):
 	return {
